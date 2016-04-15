3e3428fb1d2d4770232456ae98c1575378049c02
gingerspeed3 <nhintz7mgte@gmail.com>
2016-04-14 19:11:16 -0700

    Committing FEWD 13 lab

--- Extra Bonus Super Fun Time/RockPaperScissors/js/app.js
+++ Extra Bonus Super Fun Time/RockPaperScissors/js/app.js
@@ -1,8 +1,42 @@
+var choices = ["Rock", "Paper", "Scissors"];
+
 $(document).ready(function() {
 
-  
+  $('#cpurock, #cpupaper, #cpuscissors').hide();
+
+  function chooseRock() {
+    $('#paper, #scissors').hide();
+    var randomChoice = Math.floor(Math.random() * choices.length);
+    var cpuChoice = (choices[randomChoice]);
+
+    console.log('You chose rock');
+    console.log(cpuChoice);
+
+    if (cpuChoice == "Rock") {
+      console.log("It's a tie");
+    }  else if (cpuChoice == "Paper") {
+        console.log(Computer wins);
+    }  else if (cpuChoice == "Scissors") {
+        console.log(You win);
+    };
+  };
+
+  function choosePaper() {
+    $('#rock, #scissors').hide();
+    console.log('You chose paper');
+  };
+
+  function chooseScissors() {
+    $('#rock, #paper').hide();
+    console.log('You chose scissors');
+  };
 
 
 
+  $('#rock').click(chooseRock);
+
+  $('#paper').click(choosePaper);
+
+  $('#scissors').click(chooseScissors);
 
 });
