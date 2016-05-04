578781f277ff83b7533c2df692be71f6b1d47a2c
gingerspeed3 <nhintz7mgte@gmail.com>
2016-05-03 22:11:25 -0700

    adds changes to final project

--- Final Project/Team 2-5 RSM/js/app.js
+++ Final Project/Team 2-5 RSM/js/app.js
@@ -1,7 +1,9 @@
-console.log("Is this thing on?")
+$(document).ready(function () {
 
-$(document).ready(function() {
-  
+  $('.hidden').fadeIn(500).removeClass('hidden');
 
+  $('li').hover(function() {
+    $(this).toggleClass('animated pulse')
+  });
 
 });
